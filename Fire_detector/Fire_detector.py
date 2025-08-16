e04b818249d1f6fdc79ad5b40ec9ff47fdde6144
Ismail Malik <feranmimalik2@gmail.com>
2025-08-16 01:17:02 -0700

    Add files via upload

--- Fire_detector/Fire_detector.py
+++ Fire_detector/Fire_detector.py
@@ -1,15 +1,15 @@
 import cv2
 import streamlit as st
 import numpy as np
-st.title("Fire detector")
-fire_cascade = cv2.CascadeClassifier("fire_cascade.xml")
+st.title("Fire_detector")
+fire_cascade = cv2.CascadeClassifier("haar_cascade/fire_cascade.xml")
 uploaded_file = st.file_uploader("Upload an image or Video",type=["jpg","jpeg","png","mp4"])
 if uploaded_file is not None:
     if uploaded_file.type.startswith("image"):
         file_bytes = np.asarray(bytearray(uploaded_file.read()),dtype=np.uint8)
         img = cv2.imdecode(file_bytes,1)
         gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        fire = fire_cascade.detectMultiScale(gray, 1.1, 5)
+        fire = fire_cascade.detectMultiScale(img, 1.1, 5)
         for (x, y, w, h) in fire:
             cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 3)
         st.image(img,channels="BGR",caption="Detected Fire")
@@ -23,16 +23,7 @@ if uploaded_file is not None:
             if not _:
                 break
             gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-            if fire_cascade.empty():
-                st.error("Error while loading")
-                break
-            try:
-                 fire = fire_cascade.detectMultiScale(gray,1.1,5)
-            except Exception as e:
-                st.error(f"Detection error: {e}")
-                break
+            fire = fire_cascade.detectMultiScale(gray,1.1,5)
             for (x, y, w, h) in fire:
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 3)
             stframe.image(frame,channels="BGR")
-
-
